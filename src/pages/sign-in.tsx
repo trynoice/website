a801b911e95cec8eb4e2d9e3bcb9b953c6da7df7
Ashutosh Gangwar <ashutoshgngwr@gmail.com>
2022-06-16 04:04:28 +0530

    add a placeholder page for sign-in links

---
+++ src/pages/sign-in.tsx
@@ -0,0 +1,44 @@
+import { Link, Text } from "@chakra-ui/react";
+import { ReactElement } from "react";
+import DocumentPage from "../components/document-page";
+
+export default function RedirectPage({ location }: any): ReactElement {
+  const params = new URLSearchParams(location.search);
+  const token = params.get("token");
+
+  return (
+    <DocumentPage
+      title="Sign In"
+      meta={[
+        {
+          name: "robots",
+          content: "noindex",
+        },
+      ]}
+    >
+      <Text textAlign={"center"}>
+        Please open the sign-in link using the Noice Android app. If you're
+        already on the same device as the Noice app, please follow{" "}
+        <Link
+          as={"a"}
+          href={`noice://sign-in?token=${token}`}
+          color={"primary.500"}
+        >
+          this link
+        </Link>{" "}
+        to complete the sign-in process.
+        <br />
+        <br />
+        If this link doesn't work on your device, please inform us at{" "}
+        <Link
+          as={"a"}
+          href={"mailto:trynoiceapp@gmail.com"}
+          color={"primary.500"}
+        >
+          trynoiceapp@gmail.com
+        </Link>
+        .
+      </Text>
+    </DocumentPage>
+  );
+}
