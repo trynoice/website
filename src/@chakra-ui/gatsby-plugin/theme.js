24c1a25f22421c31e267a63577dd322d866c1ea8
Ashutosh Gangwar <ashutoshgngwr@gmail.com>
2023-02-28 23:10:23 +0530

    fix attempted import warning during gatsby build

--- src/@chakra-ui/gatsby-plugin/theme.js
+++ src/@chakra-ui/gatsby-plugin/theme.js
@@ -10,6 +10,15 @@ import "@fontsource/poppins/latin-700.css";
 import "@fontsource/urbanist/latin-500-italic.css";
 import "@fontsource/urbanist/latin-500.css";
 
+// Since we're shadowing a file from the plugin, we also need to export anything
+// else that the original file exported to prevent the following warning during
+// Gatsby build.
+//
+// `Attempted import error: 'baseTheme' is not exported from './theme' (imported as 'baseTheme').`
+//
+// https://github.com/chakra-ui/chakra-ui/issues/7179#issuecomment-1367307044
+export const baseTheme = {};
+
 export default extendTheme({
   colors: {
     black: "#1d1d26",
